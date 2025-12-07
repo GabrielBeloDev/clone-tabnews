28031f59b60883b8f05a134d92a8b98c33e5afe1
gabriel belo <gabrielbelodev@outlook.com>
2025-12-07 18:09:44 -0300

    chore: update package dependencies and database configuration

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,11 @@
-function status(req, res) {
+import database from "../../../../infra/database";    
+
+async function status(req, res) {
+    const result = await database.query('SELECT 1+1 as result');
+    console.log(result);
     res.status(200).json({
         status: "acima da média chama",
+        result: result.rows[0].result,
     });
 }
 
