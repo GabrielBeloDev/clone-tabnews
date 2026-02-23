eb2d405e51c3c9a200fcf55dacfec6a8b0ffe6d3
gabriel belo <gabrielbelodev@outlook.com>
2026-02-23 01:49:56 -0300

    refactor: clear database in test orchestrator using direct SQL queries instead of internal method

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -1,4 +1,5 @@
 import retry from "async-retry";
+import database from "infra/database.js";
 
 async function waitForAllServices() {
   await waitForWebServer();
@@ -20,7 +21,7 @@ async function waitForAllServices() {
 }
 
 async function clearDatabase() {
-  await orchestrator.clearDatabase();
+  await database.query("drop schema public cascade; create schema public;");
 }
 
 const orchestrator = {
