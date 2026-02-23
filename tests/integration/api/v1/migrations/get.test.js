c20ccef55ab3689fdff16ad327f69012bd9458df
gabriel belo <gabrielbelodev@outlook.com>
2026-02-23 01:39:33 -0300

    refactor: clarify test description for retrieving pending migrations

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -2,7 +2,6 @@ import orchestrator from "tests/orchestrator.js";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
-
   await orchestrator.clearDatabase();
 });
 
--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -2,13 +2,12 @@ import orchestrator from "tests/orchestrator.js";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
-
   await orchestrator.clearDatabase();
 });
 
 describe("POST /api/v1/migrations", () => {
   describe("Anonymous user", () => {
-    describe("Running pending migrations", () => {
+    describe("Retrieving pending migrations", () => {
       test("For the first time", async () => {
         const response1 = await fetch(
           "http://localhost:3000/api/v1/migrations",
