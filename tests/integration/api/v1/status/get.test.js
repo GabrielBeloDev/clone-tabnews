871d5d0e2367dd76ac3a5dc3bde2750daf6b8291
gabriel belo <gabrielbelodev@outlook.com>
2026-02-23 01:29:43 -0300

    refactor: improve organization and descriptions of integration tests

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,18 +4,22 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
+      const responseBody = await response.json();
 
-  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
-  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+      const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+      expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  expect(responseBody.dependecies.database.version).toMatch(/^16/);
-  expect(
-    responseBody.dependecies.database.max_connections,
-  ).toBeGreaterThanOrEqual(1);
-  expect(responseBody.dependecies.database.open_connections).toEqual(1);
+      expect(responseBody.dependecies.database.version).toMatch(/^16/);
+      expect(
+        responseBody.dependecies.database.max_connections,
+      ).toBeGreaterThanOrEqual(1);
+      expect(responseBody.dependecies.database.open_connections).toEqual(1);
+    });
+  });
 });
