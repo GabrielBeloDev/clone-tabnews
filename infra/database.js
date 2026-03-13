2bbb2b343bef2215e3c63ddb1a6c930e75e0c155
gabriel belo <gabrielbelodev@outlook.com>
2026-03-13 02:53:07 -0300

    feat: enhance error handling with ServiceError class and improve query error reporting

--- infra/database.js
+++ infra/database.js
@@ -1,4 +1,5 @@
 import { Client } from "pg";
+import { ServiceError } from "./errors.js";
 
 const database = {
   query,
@@ -12,9 +13,12 @@ async function query(queryObject) {
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
-    console.log("Erro ao executar query", error);
     console.error(error);
-    throw error;
+    const serviceErrorObject = new ServiceError({
+      message: "Erro na conexão com Banco ou na Query.",
+      cause: error,
+    });
+    throw serviceErrorObject;
   } finally {
     await client?.end();
   }
