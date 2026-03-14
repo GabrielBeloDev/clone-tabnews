8a4338f52886b4591a9f7073637e24d472aafd16
gabriel belo <gabrielbelodev@outlook.com>
2026-03-14 02:47:25 -0300

    refactor: implement next-connect for API routing and centralize API error handling

--- infra/database.js
+++ infra/database.js
@@ -13,7 +13,6 @@ async function query(queryObject) {
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
-    console.error(error);
     const serviceErrorObject = new ServiceError({
       message: "Erro na conexão com Banco ou na Query.",
       cause: error,
